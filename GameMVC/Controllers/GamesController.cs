7630b7c6adbabee8227efde6e39dbbad6333976d
lucasferreira97 <lucas97.silva@catolicasc.org.br>
2017-08-15 19:45:55 -0300

    Commit inicial

---
+++ GameMVC/Controllers/GamesController.cs
@@ -0,0 +1,20 @@
+﻿using GameMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameMVC.Controllers
+{
+    public class GamesController : Controller
+    {
+        //
+        // GET: /Games/
+        public ActionResult Index()
+        {
+            var game = new Game() { nome = "Battlefield 1" };
+            return View();
+        }
+	}
+}
---
+++ GameMVC/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameMVC.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        public String nome { get; set; }
+    }
+}
